68be4d5195b18eb884d8536bc8b2049cecf7b0e7
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-05-09 15:08:57 +0200

    feat: use github theme for code

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -1,11 +1,11 @@
-import type { ShikiSetupReturn } from '@slidev/types'
-import { defineShikiSetup } from '@slidev/types'
+import type { ShikiSetupReturn } from '@slidev/types';
+import { defineShikiSetup } from '@slidev/types';
 
 export default defineShikiSetup((): ShikiSetupReturn => {
   return {
     themes: {
-      dark: 'vitesse-dark',
-      light: 'vitesse-light',
-    },
-  }
-})
+      dark: 'github-dark-high-contrast',
+      light: 'github-light-high-contrast'
+    }
+  };
+});
