ab2d608d62aa9b83eb05fc0f8bf106bb37e4162d
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-11-24 09:55:25 +0100

    chore: intal window addon

--- components/SlideFooter.vue
+++ components/SlideFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="wave">
+    <footer class="workshops-de-gradient">
         <div class="workshops-logo">
             <img src="/workshops-logo.svg" alt="workshops.de logo" />
         </div>
@@ -7,19 +7,19 @@
 </template>
 
 <style>
-.wave {
+.workshops-de-gradient {
   display: grid;
   place-items: center;
   position: fixed;
   bottom: 0;
   left: 0;
-  height: 50px;
+  height: 2.5rem;
   width: 100%;
   background: linear-gradient(120deg, #3023ae, #0a50c2, #4286f4);
   background-size: cover;
 }
 
 .workshops-logo {
-  width: 130px;
+  width: 100px;
 }
 </style>
